22ff19a3916e7295cdadb1984405b48a2815b48c
Bwahwa <spacemansasha@gmail.com>
2025-02-12 05:18:10 -0700

    Got Flip Transform Controller "working"

--- gamegame/Assets/Scripts/Scripting4/FlipTransformController.cs
+++ gamegame/Assets/Scripts/Scripting4/FlipTransformController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class FlipTransformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public KeyCode key1 = KeyCode.RightArrow, key2 = KeyCode.LeftArrow;
+    public float direction1 = 0, direction2 = 180;
+    private void Update()
     {
-        
-    }
+        if (Input.GetKeyDown(key1))
+        {
+            transform.rotation = Quaternion.Euler(0, direction1, 0);
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (!Input.GetKeyDown(key2)) return;
+        transform.rotation = Quaternion.Euler(0, direction2, 0);
+
     }
 }
